4988fd13ddaf824fbddcb35017d7de7bba61f86e
Ahmad Nurokhim <nurokhima@outlook.co.id>
2023-03-25 06:51:31 +0700

    fix conv list and new conv bugs

--- main.py
+++ main.py
@@ -36,36 +36,41 @@ CREATE TABLE IF NOT EXISTS contents (
 def mainpage():
     return render_template('index.html')
 
-# Handle sent messages
+
 @socketio.on('user_message')
 def user_input(data):
     conn = sqlite3.connect("data.db")
     if data['conv_id'] == '999999':
         msg_conv_id = get_new_conv_id()
-        conn.execute("INSERT INTO conversations (conv_id) VALUES (?)", (msg_conv_id,))
+        conn.execute(
+            "INSERT INTO conversations (conv_id) VALUES (?)", (msg_conv_id,))
         conn.commit()
     else:
         msg_conv_id = data['conv_id']
-    conn.execute("INSERT INTO contents (conv_id, content_id, role, content) VALUES (?, ?, ?, ?)", (msg_conv_id, get_new_content_id(msg_conv_id), data['role'], data['msg']))
+    conn.execute("INSERT INTO contents (conv_id, content_id, role, content) VALUES (?, ?, ?, ?)",
+                 (msg_conv_id, get_new_content_id(msg_conv_id), data['role'], data['msg']))
     conn.commit()
     conn.close()
-    emit('user_message2', {'conv_id': msg_conv_id,}, broadcast=True)
+    emit('user_message2', {'conv_id': msg_conv_id, }, broadcast=True)
+
 
 @socketio.on('get_convs')
 def get_convs():
     conn = sqlite3.connect("data.db")
     result = conn.execute(f"SELECT * FROM conversations").fetchall()
-    conn.commit()
     conn.close()
     emit('get_convs2', result)
+
 
 @socketio.on('get_chat_box_contents')
 def get_chat_box_contents(data):
     conn = sqlite3.connect("data.db")
-    result = conn.execute(f"SELECT * FROM contents WHERE conv_id='{data['''conv_id''']}'").fetchall()
+    result = conn.execute(
+        f"SELECT * FROM contents WHERE conv_id='{data['''conv_id''']}'").fetchall()
     conn.close()
     emit('get_chat_box_contents2', result)
 
+
 if __name__ == '__main__':
     startup()
-    socketio.run(app, allow_unsafe_werkzeug=True, debug=True)
+    socketio.run(app, debug=True)
--- utilities.py
+++ utilities.py
@@ -2,14 +2,12 @@ import sqlite3
 def get_new_content_id(conv_id):
     conn = sqlite3.connect("data.db")
     current_content_count = conn.execute(f"SELECT COUNT(*) FROM contents WHERE conv_id = '{conv_id}'").fetchone()[0]
-    conn.commit()
     conn.close()
     return "{:0>6}".format(f"{current_content_count}")
 
 def get_new_conv_id():
     conn = sqlite3.connect("data.db")
     current_conv_count = conn.execute("SELECT COUNT(*) FROM conversations").fetchone()[0]
-    conn.commit()
     conn.close()
     return "{:0>6}".format(f"{current_conv_count}")
 
